e35fa3521e846a79e7895e10c630e9fd0dc65ad3
VENKATA MADHAVA REDDY LAKKIREDDY <19am1a0487@svrec.ac.in>
2024-07-31 22:29:01 +0530

    first commit

---
+++ js/playlist.js
@@ -0,0 +1,86 @@
+let songs = [
+  {
+    name: "Calm Down",
+    artist: "Rema",
+    img: "img01",
+    coverImg: "img01",
+    audio: "calmdown"
+  },
+  {
+    name: "G.O.A.T.",
+    artist: "Diljit Dosanjh",
+    img: "img02",
+    coverImg: "img02",
+    audio: "goat"
+  },
+  {
+    name: "Badshah · Arijit Singh",
+    artist: "Soulmate",
+    img: "img03",
+    coverImg: "img03",
+    audio: "badsh"
+  },
+  {
+    name: "Until I Found You",
+    artist: "Stephen Sanchez",
+    img: "img04",
+    coverImg: "img04",
+    audio: "until"
+  }
+  ,
+  {
+    name: "Give Me Some Sunshine",
+    artist: "Sharman Joshi and Suraj Jagan",
+    img: "img05",
+    coverImg: "img05",
+    audio: "idiot"
+  }
+  ,
+  {
+    name: "Nee prashnalu",
+    artist: "S.P. Balasubramanyam",
+    img: "image k",
+    coverImg: "image k",
+    audio: "Nee Prashnalu"
+  }
+  ,
+  {
+    name: "Jabilli Kosam",
+    artist: "S.P. Balasubramanyam",
+    img: "image j",
+    coverImg: "image j",
+    audio: "Jabilli Kosam"
+  }
+  ,
+  {
+    name: "Mana Friendalle",
+    artist: "Snehithudu",
+    img: "image s",
+    coverImg: "image s",
+    audio: "Mana Friendalle"
+  }
+  ,
+  {
+    name: "Theme of Kalki",
+    artist: "Kalki 2898AD",
+    img: "image d",
+    coverImg: "image d",
+    audio: "Theme of Kalki"
+  }
+  ,
+  {
+    name: "Toli Adugaina Padalede",
+    artist: "Snehithudu",
+    img: "image s",
+    coverImg: "image s",
+    audio: "Toli Adugaina Padalede"
+  }
+  ,
+  {
+    name: "Manasantha",
+    artist: "Snehithudu",
+    img: "downloaimage p",
+    coverImg: "downloaimage p",
+    audio: "Manasantha"
+  }
+]
