f84f74d67044121e9e55f2e302228f662bcbf64a
VENKATA MADHAVA REDDY LAKKIREDDY <85351822+LAKKIREDDY-2001@users.noreply.github.com>
2026-01-22 17:41:03 +0530

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -1,94 +1,221 @@
-let songs = [
-  {
-    name: "Calm Down",
-    artist: "Rema",
-    img: "img01",
-    coverImg: "img01",
-    audio: "calmdown"
-  },
-  {
-    name: "G.O.A.T.",
-    artist: "Diljit Dosanjh",
-    img: "img02",
-    coverImg: "img02",
-    audio: "goat"
-  },
-  {
-    name: "Badshah · Arijit Singh",
-    artist: "Soulmate",
-    img: "img03",
-    coverImg: "img03",
-    audio: "badsh"
-  },
-  {
-    name: "Until I Found You",
-    artist: "Stephen Sanchez",
-    img: "img04",
-    coverImg: "img04",
-    audio: "until"
-  }
-  ,
-  {
-    name: "Give Me Some Sunshine",
-    artist: "Sharman Joshi and Suraj Jagan",
-    img: "img05",
-    coverImg: "img05",
-    audio: "idiot"
-  }
-  ,
-  {
-    name: "Nee prashnalu",
-    artist: "S.P. Balasubramanyam",
-    img: "image k",
-    coverImg: "image k",
-    audio: "Nee Prashnalu"
-  }
-  ,
-  {
-    name: "Jabilli Kosam",
-    artist: "S.P. Balasubramanyam",
-    img: "image j",
-    coverImg: "image j",
-    audio: "Jabilli Kosam"
-  }
-  ,
-  {
-    name: "Mana Friendalle",
-    artist: "Snehithudu",
-    img: "image s",
-    coverImg: "image s",
-    audio: "Mana Friendalle"
-  }
-  ,
-  {
-    name: "Theme of Kalki",
-    artist: "Kalki 2898AD",
-    img: "image d",
-    coverImg: "image d",
-    audio: "Theme of Kalki"
-  }
-  ,
-  {
-    name: "Toli Adugaina Padalede",
-    artist: "Snehithudu",
-    img: "image s",
-    coverImg: "image s",
-    audio: "Toli Adugaina Padalede"
-  }
-  ,
-  {
-    name: "Manasantha",
-    artist: "Snehithudu",
-    img: "downloaimage p",
-    coverImg: "downloaimage p",
-    audio: "Manasantha"
-  }
-  ,
-  {
-    name: "Meesaala Pilla",
-    artist: "Snehithudu",
-    img: "downloaimage p",
-    coverImg: "downloaimage p",
-    audio: "Meesaala Pilla"
-  }
+// Playlist data with language categories
+const playlists = {
+  telugu: [
+    {
+      name: "Nee Prashnalu",
+      artist: "S.P. Balasubramanyam",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Nee Prashnalu"
+    },
+    {
+      name: "Jabilli Kosam",
+      artist: "S.P. Balasubramanyam",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Jabilli Kosam"
+    },
+    {
+      name: "Mana Friendalle",
+      artist: "Snehithudu",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Mana Friendalle"
+    },
+    {
+      name: "Theme of Kalki",
+      artist: "Kalki 2898AD",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Theme of Kalki"
+    },
+    {
+      name: "Toli Adugaina Padalede",
+      artist: "Snehithudu",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Toli Adugaina Padalede"
+    },
+    {
+      name: "Manasantha",
+      artist: "Snehithudu",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Manasantha"
+    },
+    {
+      name: "Meesaala Pilla",
+      artist: "Snehithudu",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Meesaala Pilla"
+    },
+    {
+      name: "2-Ori Vaari",
+      artist: "Sri Nandhan",
+      img: "img1",
+      coverImg: "img1",
+      audio: "2-Ori Vaari"
+    },
+    {
+      name: "Adiga Adiga",
+      artist: "Naga Chaitanya",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Adiga Adiga"
+    },
+    {
+      name: "Inkem Inkem Inkem Kaavaale",
+      artist: "Sid Sriram",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Inkem Inkem Inkem Kaavaale"
+    },
+    {
+      name: "Karthika Deepam",
+      artist: "Bheems Ceciroleo",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Karthika Deepam Remix"
+    },
+    {
+      name: "Gaali Vaaluga",
+      artist: "Armaan Malik",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Gaali Vaaluga"
+    },
+    {
+      name: "Undiporaadhey",
+      artist: "Anurag Kulkarni",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Undiporaadhey"
+    },
+    {
+      name: "Kanapadava",
+      artist: "M.M. Keeravani",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Kanapadava"
+    },
+    {
+      name: "Allantha Doorala",
+      artist: "Ilaiyaraaja",
+      img: "img1",
+      coverImg: "img1",
+      audio: "05 - Allantha Doorala"
+    },
+    {
+      name: "Bayilone Ballipalike",
+      artist: "M.M. Keeravani",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Bayilone Ballipalike"
+    }
+  ],
+  hindi: [
+    {
+      name: "Give Me Some Sunshine",
+      artist: "Sharman Joshi & Suraj Jagan",
+      img: "img1",
+      coverImg: "img1",
+      audio: "idiot"
+    },
+    {
+      name: "Teliyade Teliyade",
+      artist: "S.P. Balasubramanyam",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Teliyade Teliyade"
+    },
+    {
+      name: "Calm Down",
+      artist: "Rema",
+      img: "img1",
+      coverImg: "img1",
+      audio: "calmdown"
+    },
+    {
+      name: "Badshah · Arijit Singh",
+      artist: "Soulmate",
+      img: "img1",
+      coverImg: "img1",
+      audio: "badsh"
+    },
+    {
+      name: "Hook Step",
+      artist: "Hiphop Tamizha",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Hook Step"
+    }
+  ],
+  english: [
+    {
+      name: "Until I Found You",
+      artist: "Stephen Sanchez",
+      img: "img1",
+      coverImg: "img1",
+      audio: "until"
+    },
+    {
+      name: "Gira Gira",
+      artist: "Shreya Ghoshal",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Gira Gira Gingiraagirey"
+    },
+    {
+      name: "GlobeTrotter",
+      artist: "A.R. Rahman",
+      img: "img1",
+      coverImg: "img1",
+      audio: "GlobeTrotter"
+    },
+    {
+      name: "Mega Victory Mass",
+      artist: "Thaman S",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Mega Victory Mass"
+    },
+    {
+      name: "Sayyare",
+      artist: "Amit Trivedi",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Sayyare"
+    }
+  ],
+  punjabi: [
+    {
+      name: "G.O.A.T.",
+      artist: "Diljit Dosanjh",
+      img: "img1",
+      coverImg: "img1",
+      audio: "goat"
+    },
+    {
+      name: "Bhalle Bhalle",
+      artist: "Sukhwinder Singh",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Bhalle Bhalle"
+    },
+    {
+      name: "Na Prema Kathaku",
+      artist: "Sri Ranga",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Na Prema Kathaku"
+    },
+    {
+      name: "Naatu Naatu",
+      artist: "Rahul Sipligunj",
+      img: "img1",
+      coverImg: "img1",
+      audio: "Naatu Naatu"
+    }
+};
 ]
