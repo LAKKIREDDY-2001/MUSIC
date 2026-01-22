ee033616bf8207f0c9579cfa158efa659a07d5fb
VENKATA MADHAVA REDDY LAKKIREDDY <85351822+LAKKIREDDY-2001@users.noreply.github.com>
2026-01-22 18:01:41 +0530

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -1,247 +1,77 @@
-// Playlist data with language categories - Updated to match actual files
 const playlists = {
-  telugu: [
+  songs: [
     {
-      name: "Nee Prashnalu",
-      artist: "S.P. Balasubramanyam",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Nee Prashnalu"
-    },
-    {
-      name: "Jabilli Kosam",
-      artist: "S.P. Balasubramanyam",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Jabilli Kosam"
-    },
-    {
-      name: "Mana Friendalle",
-      artist: "Snehithudu",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Mana Friendalle"
-    },
-    {
-      name: "Theme of Kalki",
-      artist: "Kalki 2898AD",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Theme of Kalki"
-    },
-    {
-      name: "Toli Adugaina Padalede",
-      artist: "Snehithudu",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Toli Adugaina Padalede"
-    },
-    {
-      name: "Manasantha",
-      artist: "Snehithudu",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Manasantha"
-    },
-    {
-      name: "Meesaala Pilla",
-      artist: "Snehithudu",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Meesaala Pilla"
-    },
-    {
-      name: "2-Ori Vaari",
-      artist: "Sri Nandhan",
-      img: "img1",
-      coverImg: "img1",
-      audio: "2-Ori Vaari"
-    },
-    {
-      name: "Adiga Adiga",
-      artist: "Naga Chaitanya",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Adiga Adiga"
-    },
-    {
-      name: "Inkem Inkem Inkem Kaavaale",
-      artist: "Sid Sriram",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Inkem Inkem Inkem Kaavaale - SenSongsMp3.Co"
-    },
-    {
-      name: "Karthika Deepam",
-      artist: "Bheems Ceciroleo",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Karthika-Deepam-Remix-Song-Bheems-Ceciroleo-NaaSongs"
-    },
-    {
-      name: "Gaali Vaaluga",
-      artist: "Armaan Malik",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Gaali Vaaluga - SenSongsMp3.Co"
-    },
-    {
-      name: "Undiporaadhey",
-      artist: "Anurag Kulkarni",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Undiporaadhey - SenSongsMp3.Co"
-    },
-    {
-      name: "Kanapadava",
-      artist: "M.M. Keeravani",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Kanapadava"
-    },
-    {
-      name: "Allantha Doorala",
-      artist: "Ilaiyaraaja",
-      img: "img1",
-      coverImg: "img1",
-      audio: "05 - Allantha Doorala  - SenSongsMp3.co"
-    },
-    {
-      name: "Bayilone Ballipalike",
-      artist: "M.M. Keeravani",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Bayilone Ballipalike"
-    }
-  ],
-  hindi: [
-    {
-      name: "Give Me Some Sunshine",
-      artist: "Sharman Joshi & Suraj Jagan",
-      img: "img1",
-      coverImg: "img1",
-      audio: "idiot"
-    },
-    {
-      name: "Teliyade Teliyade",
-      artist: "S.P. Balasubramanyam",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Teliyade Teliyade - SenSongsMp3.Com"
-    },
-    {
-      name: "Calm Down",
-      artist: "Rema",
-      img: "img1",
-      coverImg: "img1",
-      audio: "calmdown"
-    },
-    {
-      name: "Badshah · Arijit Singh",
-      artist: "Soulmate",
-      img: "img1",
-      coverImg: "img1",
-      audio: "badsh"
-    },
-    {
-      name: "Hook Step",
-      artist: "Hiphop Tamizha",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Hook Step"
-    }
-  ],
-  english: [
-    {
-      name: "Until I Found You",
-      artist: "Stephen Sanchez",
-      img: "img1",
-      coverImg: "img1",
-      audio: "until"
-    },
-    {
-      name: "Gira Gira",
-      artist: "Shreya Ghoshal",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Gira Gira Gingiraagirey"
-    },
-    {
-      name: "GlobeTrotter",
-      artist: "A.R. Rahman",
-      img: "img1",
-      coverImg: "img1",
-      audio: "GlobeTrotter"
-    },
-    {
-      name: "Mega Victory Mass",
+      name: "Akhanda Thaandavam",
       artist: "Thaman S",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Mega Victory Mass"
+      img: "akhanda1",
+      coverImg: "akhanda1",
+      audio: "Akhanda Thaandavam"
     },
     {
-      name: "Sayyare",
-      artist: "Amit Trivedi",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Sayyare"
-    }
-  ],
-  punjabi: [
-    {
-      name: "G.O.A.T.",
-      artist: "Diljit Dosanjh",
-      img: "img1",
-      coverImg: "img1",
-      audio: "goat"
+      name: "Gangadhara Shankara",
+      artist: "Thaman S",
+      img: "akhanda2",
+      coverImg: "akhanda2",
+      audio: "Gangadhara Shankara"
     },
     {
-      name: "Bhalle Bhalle",
-      artist: "Sukhwinder Singh",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Bhalle Bhalle"
+      name: "Shiva Shiva",
+      artist: "Thaman S",
+      img: "akhanda3",
+      coverImg: "akhanda3",
+      audio: "Shiva Shiva"
     },
     {
-      name: "Na Prema Kathaku",
-      artist: "Sri Ranga",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Na Prema Kathaku - SenSongsmp3.Co"
+      name: "Hara Hara",
+      artist: "Thaman S",
+      img: "akhanda4",
+      coverImg: "akhanda4",
+      audio: "Hara Hara"
     },
     {
-      name: "Naatu Naatu",
-      artist: "Rahul Sipligunj",
-      img: "img1",
-      coverImg: "img1",
-      audio: "Fear"
+      name: "Shankara Shankara",
+      artist: "Thaman S",
+      img: "akhanda5",
+      coverImg: "akhanda5",
+      audio: "Shankara Shankara"
+    },
+    {
+      name: "Shambho",
+      artist: "Adviteeya Vojjala",
+      img: "akhanda6",
+      coverImg: "akhanda6",
+      audio: "Shambho"
+    },
+    {
+      name: "Akhanda Haindhavam",
+      artist: "Thaman S",
+      img: "akhanda7",
+      coverImg: "akhanda7",
+      audio: "Akhanda Haindhavam"
+    },
+    {
+      name: "The Thaandavam",
+      artist: "Thaman S",
+      img: "akhanda8",
+      coverImg: "akhanda8",
+      audio: "The Thaandavam"
+    },
+    {
+      name: "Jajikaya Jajikaya",
+      artist: "Brijesh Shandilya",
+      img: "akhanda9",
+      coverImg: "akhanda9",
+      audio: "Jajikaya Jajikaya"
     }
   ]
 };
 
-// Current filtered songs array
-let currentSongs = [];
-let currentLanguage = 'all';
+// Current songs
+let currentSongs = playlists.songs;
+let currentLanguage = "all";
 
-// Initialize with all songs combined
-function initializePlaylist() {
-  currentSongs = [
-    ...playlists.telugu,
-    ...playlists.hindi,
-    ...playlists.english,
-    ...playlists.punjabi
-  ];
-  currentLanguage = 'all';
-}
-
-// Call initialization
-initializePlaylist();
-
-// Export for use in other scripts
+// Export
 window.playlists = playlists;
 window.currentSongs = currentSongs;
 window.currentLanguage = currentLanguage;
 window.lyricsDatabase = {};
-window.initializePlaylist = initializePlaylist;
-
