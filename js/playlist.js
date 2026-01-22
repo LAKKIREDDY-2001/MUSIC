fde9bac6e38b17d55906b22ebad1bdd6870e8280
VENKATA MADHAVA REDDY LAKKIREDDY <85351822+LAKKIREDDY-2001@users.noreply.github.com>
2026-01-22 18:09:35 +0530

    Update playlist.js

--- js/playlist.js
+++ js/playlist.js
@@ -1,77 +1,199 @@
+// Main songs array for script.js to access
+let songs = [
+  {
+    name: "2-Ori Vaari",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "2-Ori Vaari"
+  },
+  {
+    name: "Allantha Doorala",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "05 - Allantha Doorala  - SenSongsMp3.co"
+  },
+  {
+    name: "Adiga Adiga",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Adiga Adiga"
+  },
+  {
+    name: "Badsh",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "badsh"
+  },
+  {
+    name: "Bayilone Ballipalike",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Bayilone Ballipalike"
+  },
+  {
+    name: "Bhalle Bhalle",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Bhalle Bhalle"
+  },
+  {
+    name: "Calm Down",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "calmdown"
+  },
+  {
+    name: "Fear",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Fear"
+  },
+  {
+    name: "Gaali Vaaluga",
+    artist: "Thaman S",
+    img: "img1",
+    audio: "Gaali Vaaluga - SenSongsMp3.Co"
+  },
+  {
+    name: "Gira Gira Gingiraagirey",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Gira Gira Gingiraagirey"
+  },
+  {
+    name: "GlobeTrotter",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "GlobeTrotter"
+  },
+  {
+    name: "Goat",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "goat"
+  },
+  {
+    name: "Hook Step",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Hook Step"
+  },
+  {
+    name: "Idiot",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "idiot"
+  },
+  {
+    name: "Inkem Inkem Inkem Kaavaale",
+    artist: "Gopi Sundar",
+    img: "img1",
+    audio: "Inkem Inkem Inkem Kaavaale - SenSongsMp3.Co"
+  },
+  {
+    name: "Jabilli Kosam",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Jabilli Kosam"
+  },
+  {
+    name: "Kanapadava",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Kanapadava"
+  },
+  {
+    name: "Karthika Deepam Remix",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Karthika Deepam Remix"
+  },
+  {
+    name: "Mana Friendalle",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Mana Friendalle"
+  },
+  {
+    name: "Manasantha",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Manasantha"
+  },
+  {
+    name: "Meesaala Pilla",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Meesaala Pilla"
+  },
+  {
+    name: "Mega Victory Mass",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Mega Victory Mass"
+  },
+  {
+    name: "Na Prema Kathaku",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Na Prema Kathaku - SenSongsmp3.Co"
+  },
+  {
+    name: "Nee Prashnalu",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Nee Prashnalu"
+  },
+  {
+    name: "Sayyare",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Sayyare"
+  },
+  {
+    name: "Teliyade Teliyade",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Teliyade Teliyade - SenSongsMp3.Com"
+  },
+  {
+    name: "Theme of Kalki",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Theme of Kalki"
+  },
+  {
+    name: "Toli Adugaina Padalede",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "Toli Adugaina Padalede"
+  },
+  {
+    name: "Undiporaadhey",
+    artist: "Anirudh Ravichander",
+    img: "img1",
+    audio: "Undiporaadhey - SenSongsMp3.Co"
+  },
+  {
+    name: "Until",
+    artist: "Unknown Artist",
+    img: "img1",
+    audio: "until"
+  }
+];
+
 const playlists = {
-  songs: [
-    {
-      name: "Akhanda Thaandavam",
-      artist: "Thaman S",
-      img: "akhanda1",
-      coverImg: "akhanda1",
-      audio: "Akhanda Thaandavam"
-    },
-    {
-      name: "Gangadhara Shankara",
-      artist: "Thaman S",
-      img: "akhanda2",
-      coverImg: "akhanda2",
-      audio: "Gangadhara Shankara"
-    },
-    {
-      name: "Shiva Shiva",
-      artist: "Thaman S",
-      img: "akhanda3",
-      coverImg: "akhanda3",
-      audio: "Shiva Shiva"
-    },
-    {
-      name: "Hara Hara",
-      artist: "Thaman S",
-      img: "akhanda4",
-      coverImg: "akhanda4",
-      audio: "Hara Hara"
-    },
-    {
-      name: "Shankara Shankara",
-      artist: "Thaman S",
-      img: "akhanda5",
-      coverImg: "akhanda5",
-      audio: "Shankara Shankara"
-    },
-    {
-      name: "Shambho",
-      artist: "Adviteeya Vojjala",
-      img: "akhanda6",
-      coverImg: "akhanda6",
-      audio: "Shambho"
-    },
-    {
-      name: "Akhanda Haindhavam",
-      artist: "Thaman S",
-      img: "akhanda7",
-      coverImg: "akhanda7",
-      audio: "Akhanda Haindhavam"
-    },
-    {
-      name: "The Thaandavam",
-      artist: "Thaman S",
-      img: "akhanda8",
-      coverImg: "akhanda8",
-      audio: "The Thaandavam"
-    },
-    {
-      name: "Jajikaya Jajikaya",
-      artist: "Brijesh Shandilya",
-      img: "akhanda9",
-      coverImg: "akhanda9",
-      audio: "Jajikaya Jajikaya"
-    }
-  ]
+  songs: songs
 };
 
 // Current songs
-let currentSongs = playlists.songs;
+let currentSongs = songs;
 let currentLanguage = "all";
 
 // Export
+window.songs = songs;
 window.playlists = playlists;
 window.currentSongs = currentSongs;
 window.currentLanguage = currentLanguage;
 window.lyricsDatabase = {};
+
